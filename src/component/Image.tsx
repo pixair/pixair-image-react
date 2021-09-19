44205c19a78d669e6fc1592bda78831763ad62dc
Vincent Barrault <vincentb45@gmail.com>
2021-09-20 01:10:03 +0200

    feat(url): fix url generation (#8)
    * fix: fix url construction, fix tests, add quality
    
    * fix: self review, add test
    
    * fix: coding style

--- src/component/Image.tsx
+++ src/component/Image.tsx
@@ -2,17 +2,55 @@ import React, { useContext } from 'react'
 import { ConfigurationContext } from '../context/ConfigurationProvider';
 
 export interface ImageProps {
+
+    /**
+     * Image source, could be an absolute or a relative path.
+     */
     src: string;
+
+    /**
+     * Set the width of the image element.
+     */
+    width: number;
+
+    /**
+     * Set the height of the image element.
+     */
+    height: number;
+
+    /**
+     * Tha alt text of the image element.
+     */
+    alt?: string;
+
+    /**
+     * Override the api option given by the ConfigurationProvider.
+     */
+    api?: string;
+
+    /**
+     * Override the quality option given by the ConfigurationProvider.
+     */
+    quality?: number;
 }
 
-export const Image = ({ src }: ImageProps) => {
+/**
+ * Image component, automatically optimize the content of the picture by using the pixair api.
+ */
+export const Image = ({
+    src,
+    width,
+    height,
+    quality,
+    api,
+    ...imageAttibutes
+}: ImageProps) => {
     const configuration = useContext(ConfigurationContext);
-    const newSrc = (src[0] === '/')
-        ? configuration.host + src
-        : configuration.host + '/' + src;
+    const apiUrl = api ?? configuration.api;
+    const imageQuality = quality ?? configuration.quality;
+    const imageSrc = `${apiUrl}?url=${src}&w=${width}&q=${imageQuality}`;
+
     return (
-        <div className="wrapper">
-            <img src={newSrc} />
-        </div>
+        <img src={imageSrc} {...imageAttibutes} />
     )
 }
--- tests/Image.test.tsx
+++ tests/Image.test.tsx
@@ -7,10 +7,54 @@ describe('The image component', () => {
     it('Should be able to render', () => {
 
         // WHEN
-        const { container } = render(<Image src='/path/to/image.png' />);
+        const { container } = render(<Image src='/path/to/image.png' width={50} height={50} />);
 
         // THEN
-        const node = container.querySelector('img');
-        expect(node?.getAttribute('src')).toEqual('https://api.pixair.io/path/to/image.png');
+        expect(container.querySelector('img')?.getAttribute('src')).toEqual('/images?url=/path/to/image.png&w=50&q=75');
+    });
+    
+    it('Should be able to have a relative path', () => {
+
+        // WHEN
+        const { container } = render(<Image src='../../path/to/image.png' width={50} height={50} />);
+
+        // THEN
+        expect(container.querySelector('img')?.getAttribute('src')).toEqual('/images?url=../../path/to/image.png&w=50&q=75');
+    });
+    
+    it('Should be able to have an absolute path', () => {
+
+        // WHEN
+        const { container } = render(<Image src='http://my-domain/path/to/image.png' width={50} height={50} />);
+
+        // THEN
+        expect(container.querySelector('img')?.getAttribute('src')).toEqual('/images?url=http://my-domain/path/to/image.png&w=50&q=75');
+    });
+    
+    it('Should be able to have a alt attribute', () => {
+
+        // WHEN
+        const { container } = render(<Image src='/path/to/image.png' width={50} height={50} alt="my-image" />);
+
+        // THEN
+        expect(container.querySelector('img')?.getAttribute('alt')).toEqual('my-image');
+    });
+    
+    it('Should be able to override the api options of the configuration', () => {
+
+        // WHEN
+        const { container } = render(<Image src='/path/to/image.png' width={50} height={50} api="https://custom-host" />);
+
+        // THEN
+        expect(container.querySelector('img')?.getAttribute('src')).toEqual('https://custom-host?url=/path/to/image.png&w=50&q=75');
+    });
+    
+    it('Should be able to override the quality options of the configuration', () => {
+
+        // WHEN
+        const { container } = render(<Image src='/path/to/image.png' width={50} height={50} quality={85} />);
+
+        // THEN
+        expect(container.querySelector('img')?.getAttribute('src')).toEqual('/images?url=/path/to/image.png&w=50&q=85');
     });
 });
